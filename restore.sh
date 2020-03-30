84d315d3f1aaed2489ce80d8f50b606bb76ffa8e
ulikoenig <github@bills-erben.de>
2020-03-30 12:23:31 +0200

    Update vom 2020-03-30

--- restore.sh
+++ restore.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #git checkout https://github.com/ulikoenig/SARS-COV-2.git
+echo Update GIT
 git pull
 
 restoreFile(){
 TODAY=`date --iso-8601`
+echo Wiederherstellung $1
 cd $1.xlsx
-zip -r ../$1.xlsx.zip *
+zip -q -9 -r ../$1.xlsx.zip *
 cd ..
 mv $1.xlsx.zip $1-restore-$TODAY.xlsx
+echo Erledigt
 }
 
 restoreFile "SH"
+restoreFile "RKI"
