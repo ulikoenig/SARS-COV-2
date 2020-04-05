eb88fb95062f85bfab8fe09d16d7b64f83fcb2bc
Uli König <github@bills-erben.de>
2020-04-05 12:04:02 +0200

    CSV Update von So 5. Apr 12:04:00 CEST 2020

--- update-csv.sh
+++ update-csv.sh
@@ -4,9 +4,12 @@ echo Update Git
 git pull
 
 echo Hole CSV von RKI
-rm RKI-CSV.csv
-wget -O RKI-CSV.csv  https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv 
+until wget -O RKI-CSV.csv.tmp  https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv 
+do 
+	sleep 10
+done 
 
+mv RKI-CSV.csv.tmp RKI-CSV.csv
 echo Upload
 git add RKI-CSV.csv
 git add update-csv.sh
