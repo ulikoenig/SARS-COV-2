707abce350fd9bb323d8fc6be43109f52a077b4a
ulikoenig <github@bills-erben.de>
2020-04-08 16:03:57 +0200

    Update vom 2020-04-08

--- update-csv.sh
+++ update-csv.sh
@@ -10,11 +10,19 @@ do
 	sleep 10
 done 
 
+echo Hole HTML von STK
+until wget -O "./STK-Quelle/`date --iso-8601`-STK.html"  https://www.schleswig-holstein.de/DE/Landesregierung/I/Presse/_documents/Corona-Liste_Kreise.html
+do 
+	sleep 10
+done 
+
+
 mv RKI-CSV.csv.tmp RKI-CSV.csv
 echo Upload
 git add RKI-CSV.csv
 git add update-csv.sh
-git commit -m "CSV Update von $TODAY - Quellenvermerk: Robert Koch-Institut (RKI), dl-de/by-2-0"
-git push
+git add STK-Quelle
+#git commit -m "CSV Update von $TODAY - Quellenvermerk: Robert Koch-Institut (RKI), dl-de/by-2-0"
+#git push
 
 echo Fertig
