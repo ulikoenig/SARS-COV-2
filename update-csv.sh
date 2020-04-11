94c344343d49a762bde42ba6afef8530f3e09148
ulikoenig <github@bills-erben.de>
2020-04-11 18:20:36 +0200

    Script Update vom Sat Apr 11 18:20:31 CEST 2020

--- update-csv.sh
+++ update-csv.sh
@@ -4,33 +4,46 @@ TODAY=`date`
 echo Update Git
 git pull
 
+
+#RKI
+
 echo Hole CSV von RKI
 until wget -O RKI-CSV.csv.tmp  https://opendata.arcgis.com/datasets/dd4580c810204019a7b8eb3e0b329dd6_0.csv 
 do 
 	sleep 10
 done 
+mv RKI-CSV.csv.tmp RKI-CSV.csv
+git add RKI-CSV.csv
+git commit -m "CSV Update von $TODAY - Quellenvermerk: Robert Koch-Institut (RKI), dl-de/by-2-0"
+
+
+#STK
 
 echo Hole HTML von STK
-until wget -O "./STK-Quelle/`date --iso-8601`-STK.html"  https://www.schleswig-holstein.de/DE/Landesregierung/I/Presse/_documents/Corona-Liste_Kreise.html
+until wget -O "./STK-Quelle/`date --iso-8601`-STK.html.tmp"  https://www.schleswig-holstein.de/DE/Landesregierung/I/Presse/_documents/Corona-Liste_Kreise.html
 do 
 	sleep 10
 done 
-
-echo Hole HTML von Divi
-until wget -O "./divi/`date --iso-8601`-divi.html" https://diviexchange.z6.web.core.windows.net/
-do 
-	sleep 10
-done 
-
-
-mv RKI-CSV.csv.tmp RKI-CSV.csv
-echo Upload
-git add RKI-CSV.csv
-git add update-csv.sh
-git commit -m "CSV Update von $TODAY - Quellenvermerk: Robert Koch-Institut (RKI), dl-de/by-2-0"
-git push
+mv "./STK-Quelle/`date --iso-8601`-STK.html.tmp" "./STK-Quelle/`date --iso-8601`-STK.html"
 git add STK-Quelle
 git commit -m "HTML Update von $TODAY - Quellenvermerk: Staatskanzlei SH"
+
+
+#DIVI
+
+echo Hole HTML von Divi
+until wget -O "./divi-Quelle/`date --iso-8601`-divi.html.tmp" https://diviexchange.z6.web.core.windows.net/
+do 
+	sleep 10
+done 
+mv "./divi-Quelle/`date --iso-8601`-divi.html.tmp" "./divi-Quelle/`date --iso-8601`-divi.html"
+git add divi-Quelle
+git commit -m "DIVI Update von $TODAY - Quellen https://diviexchange.z6.web.core.windows.net/"
+
+
+echo Upload
+git add update-csv.sh
+git commit -m "Script Update vom $TODAY"
 git push
 
 echo Fertig
